ee3d342bacbcbbf59ef23b988dcc2fa19aa46724
Alexandr Myasnikov <Skrymer1639@yandex.ru>
2023-05-04 08:19:00 +0500

    Temp

---
+++ Assets/Scripts/Entity/Objects/Inventory/DropObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropObject : MonoBehaviour
+{
+    [SerializeField] private Inventory _inventory;
+    [SerializeField] private ActionObject _actionObj;
+}
