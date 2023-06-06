3c584176a2ae36f9a15e04e2bb97f247e7ac27e9
Alexandr Myasnikov <Skrymer1639@yandex.ru>
2023-06-06 16:52:34 +0500

    colliders, default objects, shadows

---
+++ Assets/Scripts/Controllers/TriggerObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerObject : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnEnter;
+    [SerializeField] private UnityEvent OnStay;
+    [SerializeField] private UnityEvent OnExit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnEnter?.Invoke();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        OnStay?.Invoke();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        OnExit?.Invoke();
+    }
+}
